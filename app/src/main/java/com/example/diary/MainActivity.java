b920ea525560a3c2e692137bcee0abbdee7293d9
ojingjing <wlddj30807@kyungsung.ac.kr>
2023-02-10 23:47:13 +0900

    Diary commit

--- app/src/main/java/com/example/diary/MainActivity.java
+++ app/src/main/java/com/example/diary/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.diary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.write);
+        EditText editText = findViewById(R.id.diary);
+
+
+        String getString =getIntent().getStringExtra("context");
+
+        editText.setText("왜 안떠 "+ getString);
+//        Log.i("Sting-check",getString);
+
+//        startActivityForResult();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getApplication(),WriteActivity.class));
             }
         });
+
     }
 }
--- app/src/main/java/com/example/diary/WriteActivity.java
+++ app/src/main/java/com/example/diary/WriteActivity.java
@@ -1,10 +1,15 @@
 package com.example.diary;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class WriteActivity extends AppCompatActivity {
@@ -15,20 +20,41 @@ public class WriteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_write);
         Button buttonsave =findViewById(R.id.save);
         Button buttoncancel =findViewById(R.id.cancel);
-        public void m0nClick(View v){
-            AlterDialog.Builder builder;
-            switch (v.getId()){
-
-            }
-        }
         buttonsave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(WriteActivity.this, "Today saved :-)", Toast.LENGTH_SHORT).show();
+                AlertDialog.Builder builder= new AlertDialog.Builder(WriteActivity.this);
+                builder.setTitle("제목");
+                builder.setMessage("저장하시겠습니까?");
+                builder.setPositiveButton("ㅇ", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { //error
+                        data();
+                        Toast.makeText(WriteActivity.this, "Today saved :-)", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+                builder.setNegativeButton("x", null );
+                builder.create().show();
+
+
+            }
+        });
+        buttoncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
             }
         });
 
 
         }
+        void data(){
+            Intent intent =new Intent(this, MainActivity.class);
+            EditText editText = findViewById(R.id.context);
+            intent.putExtra("context",editText.getText().toString());
+            getIntent().setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        }
+
     }
-}
